bf521cf8c1b3bce210fb9cdb8a1d84db181a567e
David <David@DJOU20130704>
2016-07-20 23:42:45 -0500

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Circle
+    {
+        public static float _PI;
+        int _Radius;
+
+        static Circle()
+        {
+            Console.WriteLine("static member called");
+            Circle._PI = 3.141f;
+        }
+        public Circle(int Radius)
+        {
+            Console.WriteLine("instance member called");
+            this._Radius = Radius;
+        }
+
+    
+        public float CalculatArea()
+        {
+            return Circle._PI * this._Radius * this._Radius;
+
+        }
+    }
+
+
+
+    class Program
+    {
+        public static void Main()
+        {
+            Circle C1 = new Circle(5);
+            float Area1 = C1.CalculatArea();
+            Console.WriteLine("Area  = {0}", Area1);
+
+            Circle C2 = new Circle(15);
+            float Area2 = C2.CalculatArea();
+            Console.WriteLine("Area = {0}", Area2);
+
+        }
+    }
+
+}
